8b35a3f0f8b5dc555a5e23b23eccc61b982a8bdc
jeyson08 <jeyson.boursault@gmail.com>
2024-05-23 16:48:58 +0200

    timer

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const form = document.querySelector("form");
 const input = document.querySelector('input[type="text"]');
 const span = document.querySelector("span");
+let timer;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value === "ami") {
     span.textContent = "Les portes s'ouvrent !";
+    timer = 5000;
   } else {
     span.textContent = "Rien ne se passe ...";
+    timer = 1000;
   }
   input.value = "";
   setTimeout(() => {
     span.textContent = "";
-  }, 1000);
+  }, timer);
 });
